ebb290da04cbe353fca881e47d5840b35a6c8e55
Vitaliy <theyamshikov@yandex.ru>
2017-06-24 21:04:56 +0600

    init files

---
+++ src/yii/base/Model.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace yii2lab\misc\yii\base;
+
+use Yii;
+use yii\base\Model as YiiModel;
+use yii\db\BaseActiveRecord;
+
+class Model extends YiiModel
+{
+
+	const SCENARIO_CREATE = 'create';
+	const SCENARIO_UPDATE = 'update';
+
+	/** @var BaseActiveRecord $modelInstance */
+	protected $modelInstance;
+
+	public function modelClass()
+	{
+		return null;
+	}
+
+	public function getRules($rules = [])
+	{
+		$modelClass = $this->modelClass();
+		$modelRules = [];
+		if($modelClass) {
+			/** @var BaseActiveRecord $modelInstance */
+			$modelInstance = new $modelClass();
+			$modelRules = $modelInstance->rules();
+		}
+		return array_merge($modelRules, $rules);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		$modelClass = $this->modelClass();
+		if($modelClass) {
+			return $this->getModelAttr('attributeLabels', $modelClass);
+		}
+		return [];
+	}
+
+	/**
+	 * Return first error of model
+	 * @return string
+	 */
+	public function getFirstErrorModel()
+	{
+		foreach ($this->getFirstErrors() as $attribute => $error) {
+			return $error;
+		}
+		return null;
+	}
+
+	protected function saveModel()
+	{
+		$saveResult = $this->modelInstance->save();
+		if($this->modelInstance->hasErrors() || !$saveResult) {
+			$this->addErrors($this->modelInstance->getErrors());
+		}
+		return $saveResult ? $this->modelInstance : null;
+	}
+
+	/**
+	 * load model instance.
+	 *
+	 * @param integer|null $id
+	 *
+	 * @return BaseActiveRecord|null the saved model or null if saving fails
+	 */
+	protected function loadModel($id = null)
+	{
+		/** @var BaseActiveRecord $modelClass */
+		$modelClass = $this->modelClass();
+		if($id) {
+			$this->modelInstance = $modelClass::findOne($id);
+		} else {
+			$this->modelInstance = new $modelClass();
+		}
+		$this->modelInstance->load($this->attributes, '');
+		return $this->modelInstance;
+	}
+
+	protected function getModelAttr($method, $modelClass = null, $attrs = [])
+	{
+		if(empty($modelClass)) {
+			$modelClass = $this->modelClass();
+		}
+		$modelRules = (new $modelClass)->{$method}();
+		return array_merge($modelRules, $attrs);
+	}
+}
---
+++ src/yii/grid/ActionColumn.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace yii2lab\misc\yii\grid;
+
+use Yii;
+use yii2lab\helpers\yii\Html;
+use yii\helpers\Url;
+
+class ActionColumn extends \yii\grid\ActionColumn
+{
+	
+	public $headerOptions = ['class' => 'action-column', 'width' => '20'];
+	
+	public $buttonOptions = ['class' => ['btn btn-xs']];
+	
+	public $template = '{view} {update} {delete}';
+   
+	/* public function init()
+	{
+		parent::init();
+		$this->initDefaultButtons();
+		$url = '/todo/task/create';
+		$options = array_merge_recursive([
+			'title' => Yii::t('main', 'create'),
+			'aria-label' => Yii::t('main', 'create'),
+			'data-pjax' => '0',
+		], $this->buttonOptions);
+		$this->header = Html::a(Html::fa('plus', 'success'), $url, $options);
+	} */
+
+	/**
+	 * Initializes the default button rendering callbacks.
+	 */
+	protected function initDefaultButtons()
+	{
+		if (!isset($this->buttons['view'])) {
+			$this->buttons['view'] = function ($url, $model, $key) {
+				$options = array_merge_recursive([
+					'title' => Yii::t('yii', 'View'),
+					'aria-label' => Yii::t('yii', 'View'),
+					'data-pjax' => '0',
+				], $this->buttonOptions);
+				return Html::a(Html::fa('eye'), $url, $options);
+			};
+		}
+		if (!isset($this->buttons['update'])) {
+			$this->buttons['update'] = function ($url, $model, $key) {
+				$options = array_merge_recursive([
+					'title' => Yii::t('yii', 'Update'),
+					'aria-label' => Yii::t('yii', 'Update'),
+					'data-pjax' => '0',
+					//'class' => ['btn-primary'],
+				], $this->buttonOptions);
+				return Html::a(Html::fa('pencil'), $url, $options);
+			};
+		}
+		if (!isset($this->buttons['delete'])) {
+			$this->buttons['delete'] = function ($url, $model, $key) {
+				$options = array_merge_recursive([
+					'title' => Yii::t('yii', 'Delete'),
+					'aria-label' => Yii::t('yii', 'Delete'),
+					'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+					'data-method' => 'post',
+					'data-pjax' => '0',
+					//'class' => ['btn-danger'],
+				], $this->buttonOptions);
+				return Html::a(Html::fa('trash', 'danger'), $url, $options);
+			};
+		}
+		if (!isset($this->buttons['complete'])) {
+			$this->buttons['complete'] = function ($url, $model, $key) {
+				$options = array_merge_recursive([
+					'title' => Yii::t('main', 'complete'),
+					'aria-label' => Yii::t('main', 'complete'),
+					'data-confirm' => Yii::t('main', 'complete_confirm'),
+					'data-method' => 'post',
+					'data-pjax' => '0',
+					//'class' => ['btn-success'],
+				], $this->buttonOptions);
+				return Html::a(Html::fa('check', 'success'), $url, $options);
+			};
+		}
+	}
+
+}
