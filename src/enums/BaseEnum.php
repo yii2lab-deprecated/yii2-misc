a0f8629a5855f8887a7fafdad6f878585897d7ac
Vitaliy <theyamshikov@yandex.ru>
2017-09-27 14:45:32 +0600

    create base class

---
+++ src/enums/BaseEnum.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace yii2lab\misc\enums;
+
+use yii2lab\domain\helpers\ReflectionHelper;
+
+class BaseEnum {
+	
+	public static function values($prefix = null) {
+		$constants = static::all($prefix);
+		$constants = array_values($constants);
+		$constants = array_unique($constants);
+		return $constants;
+	}
+	
+	public static function keys($prefix = null) {
+		$constants = static::all($prefix);
+		$constants = array_keys($constants);
+		return $constants;
+	}
+	
+	public static function all($prefix = null) {
+		$className = get_called_class();
+		if(!empty($prefix)) {
+			$constants = ReflectionHelper::getConstantsByPrefix($className, $prefix);
+		} else {
+			$constants = ReflectionHelper::getConstants($className);
+		}
+		return $constants;
+	}
+	
+}
