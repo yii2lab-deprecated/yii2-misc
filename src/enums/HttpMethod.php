8356ec004caccf96f7caf6eea15c14aed26dee02
Vitaliy <theyamshikov@yandex.ru>
2017-09-27 14:45:51 +0600

    create HttpMethod enum

---
+++ src/enums/HttpMethod.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace yii2lab\misc\enums;
+
+class HttpMethod extends BaseEnum {
+	
+	const GET = 'GET';
+	const POST = 'POST';
+	const PUT = 'PUT';
+	const DELETE = 'DELETE';
+	const OPTIONS = 'OPTIONS';
+	const HEAD = 'HEAD';
+	const PATCH = 'PATCH';
+	const TRACE = 'TRACE';
+	const CONNECT = 'CONNECT';
+
+}
