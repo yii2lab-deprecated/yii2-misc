35c59fce1978855e3d35979a7e64db0275ae57f1
Vitaliy <theyamshikov@yandex.ru>
2017-12-17 16:46:51 +0600

    make html enum

---
+++ src/enums/HtmlEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace yii2lab\misc\enums;
+
+class HtmlEnum extends BaseEnum {
+	
+	const PIPE = '<span class="text-muted"> | </span>';
+	const BR = '<br />';
+	const NBSP = '&nbsp;';
+
+}
