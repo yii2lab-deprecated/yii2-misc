e785ac13482b95b16d90f409fe1a2ee69c9de9d2
Vitaliy <theyamshikov@yandex.ru>
2017-12-19 00:25:18 +0600

    refacto

--- src/helpers/CommandHelper.php
+++ src/helpers/CommandHelper.php
@@ -9,12 +9,21 @@ use yii2lab\misc\interfaces\CommandInterface;
 class CommandHelper {
 	
 	public static function run($config, $class = null) {
+		$command = self::create($config, $class);
+		return $command->run();
+	}
+	
+	public static function create($config, $class = null) {
 		if(!empty($class)) {
 			$config['class'] = $class;
 		}
-		$generator = Yii::createObject($config);
-		if($generator instanceof CommandInterface) {
-			return $generator->run();
+		return self::createCommand($config);
+	}
+	
+	private static function createCommand($config) {
+		$command = Yii::createObject($config);
+		if($command instanceof CommandInterface) {
+			return $command;
 		} else {
 			throw new ServerErrorHttpException('Command not be instance of "CommandInterface"');
 		}
