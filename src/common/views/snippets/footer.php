d5e27b53354a55645efec3161a2b72b836b8dbd6
Vitaliy <theyamshikov@yandex.ru>
2017-12-19 17:28:30 +0600

    refacto

--- src/common/views/snippets/footer.php
+++ src/common/views/snippets/footer.php
@@ -1,48 +1,19 @@
 <?php
 
-use yii\helpers\Html;
+use yii2lab\helpers\MenuHelper;
 use yii2lab\helpers\ModuleHelper;
 use yii2module\article\widgets\PostList;
 use yii2module\lang\widgets\LangSelector;
+
+$menu = include(COMMON_DATA_DIR . DS . 'menu' . DS . 'footer.php');
+$items = MenuHelper::gen($menu['leftMenu']);
 
 ?>
 
 <div class="pull-left">
 	<p>
-	&copy; <?= Yii::$app->name . SPC . date('Y') ?>
-
-
-		<?php if(YII_ENV_DEV) { ?>
-			<!--
-			<span class="text-muted"> | </span>
-			<?= Html::a(t('main', 'documentation'), param('url.frontend') . 'doc') ?>
-			-->
-			<?php if(ModuleHelper::has('guide')) { ?>
-				<span class="text-muted"> | </span>
-				<?= Html::a(t('guide/main', 'title'), ['/guide']) ?>
-			<?php } ?>
-
-			<?php if(ModuleHelper::has('gii') && Yii::$app->user->can('backend.*')) { ?>
-				<span class="text-muted"> | </span>
-				<?= Html::a('Gii', ['/gii']) ?>
-			<?php } ?>
-
-			<?php if(!in_array('debug', config('bootstrap'))) { ?>
-                <span class="text-muted"> | </span>
-				<?= Html::a('Yii Debugger', ['/debug/default/view', 'panel' => 'request']) ?>
-			<?php } ?>
-   
-		<?php } ?>
-
-		<?php if(APP == FRONTEND) { ?>
-
-			<?php if(Yii::$app->user->can('backend.*')) { ?>
-				<span class="text-muted"> | </span>
-				<?= Html::a(t('main', 'go_to_backend'), param('url.backend')) ?>
-			<?php } ?>
-
-		<?php } ?>
-
+	    &copy; <?= Yii::$app->name . SPC . date('Y') ?>
+        <?= MenuHelper::renderMenu($items) ?>
 	</p>
 
 	<?php if(APP == FRONTEND) { ?>
