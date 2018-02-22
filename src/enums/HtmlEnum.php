3a944191bdef01fab9e502cd8177f85c6f390f42
Vitaliy <theyamshikov@yandex.ru>
2018-02-22 14:26:41 +0600

    make const

--- src/enums/HtmlEnum.php
+++ src/enums/HtmlEnum.php
@@ -5,6 +5,7 @@ namespace yii2lab\misc\enums;
 class HtmlEnum extends BaseEnum {
 	
 	const PIPE = '<span class="text-muted"> | </span>';
+    const CARET = '<b class="caret"></b>';
 	const BR = '<br />';
 	const NBSP = '&nbsp;';
 
