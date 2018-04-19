13d7541f5aff005ca3f46c10d5bb1e54f5244808
Vitaliy <theyamshikov@yandex.ru>
2018-04-19 15:07:38 +0600

    fix

--- src/enums/ApiVersionBaseEnum.php
+++ src/enums/ApiVersionBaseEnum.php
@@ -7,6 +7,7 @@ class ApiVersionBaseEnum extends BaseEnum {
     public static function getApiVersionNumberList()
     {
         $dirList = self::values();
+	    $result = [];
         foreach($dirList as $path) {
             if (preg_match('#v([0-9]+)#i', $path, $matches)) {
                 $result[] = $matches[1];
