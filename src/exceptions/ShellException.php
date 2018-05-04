5da96a7291228d1f0a9d9d3a71ea36a54ceed150
Vitaliy <theyamshikov@yandex.ru>
2018-05-04 19:22:57 +0600

    move exception

--- src/exceptions/ShellException.php
+++ src/exceptions/ShellException.php
@@ -4,5 +4,12 @@ namespace yii2lab\misc\exceptions;
 
 use Exception;
 
+/**
+ * Class ShellException
+ *
+ * @package yii2lab\misc\exceptions
+ *
+ * @deprecated use \yii2lab\extension\shell\base\exception\ShellException
+ */
 class ShellException extends Exception {
 }
